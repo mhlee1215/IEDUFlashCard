a77365de8b57c580ded81f38004bdacec006071a
Jaemin Park <jmpark00611@gmail.com>
2016-02-07 16:07:49 -0800

    FrontActivity added

--- app/src/main/java/com/flashcard/iedu/flashcard/FrontActivity.java
+++ app/src/main/java/com/flashcard/iedu/flashcard/FrontActivity.java
@@ -1,14 +1,20 @@
 package com.flashcard.iedu.flashcard;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
+import android.view.View;
 
-public class FrontActivity extends Activity {
+public class FrontActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front);
+    }
+
+    public void sendMessage (View view) {
+        System.out.println("Hello button");
+
     }
 
 }
