ba89fe5035d77fefe08a381ce2b50be1f2b3fe63
mhlee <mhlee1215@gmail.com>
2016-03-06 11:46:05 -0800

    Server Connected service added.

--- app/src/main/java/com/flashcard/iedu/flashcard/samples/serverConn/ServiceTestActivity.java
+++ app/src/main/java/com/flashcard/iedu/flashcard/samples/serverConn/ServiceTestActivity.java
@@ -1,13 +1,15 @@
 package com.flashcard.iedu.flashcard.samples.serverConn;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.flashcard.iedu.flashcard.R;
 
-//import edu.iedu.flashcard.service.UserService;
+import edu.iedu.flashcard.service.UserService;
 
 public class ServiceTestActivity extends AppCompatActivity {
 
@@ -15,11 +17,23 @@ public class ServiceTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_test);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 
     public void sendMessage(View view){
 
-        //System.out.println(UserService.getUsers());
+        Connection conn = new Connection();
+        conn.doInBackground();
 
+    }
+
+    private class Connection extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object... arg0) {
+            System.out.println(UserService.getUsers());
+            return null;
+        }
     }
 }
