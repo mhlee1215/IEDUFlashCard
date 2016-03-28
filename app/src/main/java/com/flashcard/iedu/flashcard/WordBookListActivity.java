11481df8404fb037f1e0c0c94fc06e5b6a1f5b10
mhlee <mhlee1215@gmail.com>
2016-03-27 18:11:47 -0700

    wordbook list updated.

--- app/src/main/java/com/flashcard/iedu/flashcard/WordBookListActivity.java
+++ app/src/main/java/com/flashcard/iedu/flashcard/WordBookListActivity.java
@@ -1,9 +1,13 @@
 package com.flashcard.iedu.flashcard;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,18 +22,32 @@ public class WordBookListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wordbook_list);
 
-        context=this;
+        context = this;
 
         List<String> mainColumn = new ArrayList<String>();
         List<String> subColumn1 = new ArrayList<String>();
 
-        for(int i = 0 ; i < 30 ; i ++){
-            mainColumn.add("main "+i);
-            subColumn1.add("sub1 "+i);
+        for (int i = 0; i < 30; i++) {
+            mainColumn.add("main " + i);
+            subColumn1.add("sub1 " + i);
         }
 
 
-        lv=(ListView) findViewById(R.id.listView);
-        lv.setAdapter(new WordBookListAdapter(this, mainColumn,subColumn1));
+        lv = (ListView) findViewById(R.id.listView);
+        lv.setAdapter(new WordBookListAdapter(this, mainColumn, subColumn1));
+    }
+
+    public void sendMessage (View view) {
+        TextView viewMain =(TextView) view.findViewById(R.id.textViewMain);
+        Toast.makeText(context, "You Clicked flashcards ", Toast.LENGTH_LONG).show();
+    }
+
+    public void sendMessage1 (View view) {
+        TextView viewMain =(TextView) view.findViewById(R.id.textViewMain);
+        Toast.makeText(context, "You clicked Learn", Toast.LENGTH_LONG).show();
+    }
+    public void sendMessage2 (View view) {
+        TextView viewMain = (TextView) view.findViewById(R.id.textViewMain);
+        Toast.makeText( context, "You clicked Match", Toast.LENGTH_LONG).show();
     }
 }
