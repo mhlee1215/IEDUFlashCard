2cb2c32e8683a4b96817315c96ab5d252b2549b6
mhlee <mhlee1215@gmail.com>
2016-04-17 15:01:50 -0700

    quiz activity added.

--- app/src/main/java/com/flashcard/iedu/flashcard/QuizActivity.java
+++ app/src/main/java/com/flashcard/iedu/flashcard/QuizActivity.java
@@ -2,6 +2,7 @@ package com.flashcard.iedu.flashcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -9,6 +10,9 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        TextView textDefinition = (TextView)findViewById(R.id.text_definition);
+        textDefinition.setText("My Definition");
     }
 
 
