2f6fc154e374cb998a193012163ba45d31ff4dc5
mhlee <mhlee1215@gmail.com>
2016-05-08 15:40:27 -0700

    server connected date added.

--- app/src/main/java/com/flashcard/iedu/flashcard/WordBookListActivity.java
+++ app/src/main/java/com/flashcard/iedu/flashcard/WordBookListActivity.java
@@ -1,7 +1,9 @@
 package com.flashcard.iedu.flashcard;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ListView;
@@ -14,25 +16,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.iedu.flashcard.dao.domain.Word;
+import edu.iedu.flashcard.service.UserService;
 import edu.iedu.flashcard.service.WordService;
 
 public class WordBookListActivity extends AppCompatActivity {
 
     ListView lv;
     Context context;
+    List<Word> wordList;
+    WordBookListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wordbook_list);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+
         context = this;
 
-        List wordList = WordService.getWordList(1);
-
+        wordList = new ArrayList<Word>();//
+        //WordService.getWordList(1);
 
         lv = (ListView) findViewById(R.id.listView);
-        lv.setAdapter(new WordBookListAdapter(this, wordList));
+
+        adapter = new WordBookListAdapter(this, wordList);
+        lv.setAdapter(adapter);
+
+        getWords();
     }
 
     public void sendMessage (View view) {
@@ -47,5 +60,20 @@ public class WordBookListActivity extends AppCompatActivity {
     public void sendMessage2 (View view) {
         TextView viewMain = (TextView) view.findViewById(R.id.textViewMain);
         Toast.makeText( context, "You clicked Match", Toast.LENGTH_LONG).show();
+    }
+
+    public void getWords(){
+        Connection conn = new Connection();
+        conn.doInBackground(1);
+    }
+
+    private class Connection extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object... arg0) {
+            List<Word> test = WordService.getWordList(1);
+            wordList.addAll(test);
+            adapter.notifyDataSetChanged();
+            return null;
+        }
     }
 }
