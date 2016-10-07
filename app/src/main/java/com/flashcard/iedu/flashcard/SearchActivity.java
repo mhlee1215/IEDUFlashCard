6f1877316c5ab66bdd53c27cfe55c570b41e9c16
mhlee <mhlee1215@gmail.com>
2016-10-07 15:51:16 -0700

    layout updated.

--- app/src/main/java/com/flashcard/iedu/flashcard/SearchActivity.java
+++ app/src/main/java/com/flashcard/iedu/flashcard/SearchActivity.java
@@ -1,9 +1,12 @@
 package com.flashcard.iedu.flashcard;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -15,63 +18,86 @@ import java.util.List;
 import edu.iedu.flashcard.dao.domain.WordBook;
 
 
-    public class SearchActivity extends AppCompatActivity {
-        Context context;
-        ListView lv;
-        List<WordBook> wordbookList;
-        SearchAdapter adapter;
+public class SearchActivity extends AppCompatActivity {
+    Context context;
+    ListView lv;
+    List<WordBook> wordbookList;
+    SearchAdapter adapter;
+    String actionBarTitle = "Wordbook Search";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
 
 
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.search_toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(actionBarTitle);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+
+        getSupportActionBar().setDisplayUseLogoEnabled(false);
 
 
-            context = this;
+        context = this;
 
-            wordbookList = new ArrayList<WordBook>();
+        wordbookList = new ArrayList<WordBook>();
 
-            lv = (ListView) findViewById(R.id.ListView);
-            adapter = new SearchAdapter(this, wordbookList);
-            lv.setAdapter(adapter);
+        lv = (ListView) findViewById(R.id.ListView);
+        adapter = new SearchAdapter(this, wordbookList);
+        lv.setAdapter(adapter);
 
-            getWordBooks();
+        getWordBooks();
 
-        }
+    }
 
-        public void sendMessage(View view) {
-            TextView viewMain = (TextView) view.findViewById(R.id.textViewMain);
-            Toast.makeText(context, "You Clicked latest ", Toast.LENGTH_LONG).show();
-        }
+    public void sendMessage(View view) {
+        TextView viewMain = (TextView) view.findViewById(R.id.textViewMain);
+        Toast.makeText(context, "You Clicked latest ", Toast.LENGTH_LONG).show();
+    }
 
-        public void sendMessage1(View view) {
-            TextView viewMain = (TextView) view.findViewById(R.id.textViewMain);
-            Toast.makeText(context, "You clicked set", Toast.LENGTH_LONG).show();
-        }
+    public void sendMessage1(View view) {
+        TextView viewMain = (TextView) view.findViewById(R.id.textViewMain);
+        Toast.makeText(context, "You clicked set", Toast.LENGTH_LONG).show();
+    }
 
-        public void sendMessage2(View view) {
-            TextView viewMain = (TextView) view.findViewById(R.id.textViewMain);
-            Toast.makeText(context, "You clicked class", Toast.LENGTH_LONG).show();
-        }
-        public void getWordBooks () {
-            Connection conn = new Connection();
-            conn.doInBackground(1);
-        }
+    public void sendMessage2(View view) {
+        TextView viewMain = (TextView) view.findViewById(R.id.textViewMain);
+        Toast.makeText(context, "You clicked class", Toast.LENGTH_LONG).show();
+    }
+    public void getWordBooks () {
+        Connection conn = new Connection();
+        conn.doInBackground(1);
+    }
 
-        private class Connection extends AsyncTask {
+    private class Connection extends AsyncTask {
 
-            protected Object doInBackground(Object... arg0) {
-                int wordBookId = (Integer)arg0[0];
-                //List<WordBook> test = WordService.getWordBookList(wordBookId);
-                List<WordBook> test = new ArrayList<WordBook>();
-                for (int i = 0; i < 30; i++) {
-                    test.add(new WordBook("dummy wordbook_" + i));
-
-                }
-                wordbookList.addAll(test);
-                adapter.notifyDataSetChanged();
-                return null;
+        protected Object doInBackground(Object... arg0) {
+            int wordBookId = (Integer)arg0[0];
+            //List<WordBook> test = WordService.getWordBookList(wordBookId);
+            List<WordBook> test = new ArrayList<WordBook>();
+            for (int i = 0; i < 30; i++) {
+                test.add(new WordBook("dummy wordbook_" + i));
 
             }
+            wordbookList.addAll(test);
+            adapter.notifyDataSetChanged();
+            return null;
+
         }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/flashcard/iedu/flashcard/WordEditActivity.java
+++ app/src/main/java/com/flashcard/iedu/flashcard/WordEditActivity.java
@@ -2,12 +2,34 @@ package com.flashcard.iedu.flashcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class WordEditActivity extends AppCompatActivity {
-
+    String actionBarTitle = "Word Edit";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_edit);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.wordedit_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(actionBarTitle);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+
+        getSupportActionBar().setDisplayUseLogoEnabled(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
